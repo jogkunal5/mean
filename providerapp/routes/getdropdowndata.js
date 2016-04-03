8d3a71c172289b4ff6e6ec75e27f840938249b72
jogkunal5 <jogkunal5@gmail.com>
2016-04-04 00:45:58 +0530

    new

---
+++ providerapp/routes/getdropdowndata.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var router=express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('providerapp');
+var url=require('url');
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json()); // To parse the body that we received from input
+
+/*************************************************************************************
+ *@Route: "/getcollectiondata"
+ *@Methods: .get
+ *************************************************************************************/
+router.route('/').get(function (req, res) {    
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    db.collection(query.collection).find(function (err, docs) {        
+        res.json(docs);
+    });
+});
+
+module.exports=router;
